9f440299fea89a640763d2f128a7ce033de88596
nikwif <149531476+nikwif@users.noreply.github.com>
2025-03-27 16:08:50 +0000

    urls hardcode proxy

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -30,11 +30,10 @@ function Dashboard() {
   };
 
   const proxyProducts: Product[] = [
-    { id: "submit-form", name: "🧑‍💻 Scraper Submit Form", type: "SERP", description: "Submit files to Dev Scraper.", owned: true, path: "/scraping-api/submit-form/google-serp" },
+    // { id: "submit-form", name: "🧑‍💻 Scraper Submit Form", type: "SERP", description: "Submit files to Dev Scraper.", owned: true, path: "/scraping-api/submit-form/google-serp" },
     { id: "explore-serp", name: "📋 Scraper Jobs", type: "SERP", description: "View Scraper Files.", owned: true, path: "/scraping-api/explore" },
     { id: "icon-gpt", name: "🤖 IconGpt", type: "AI", description: "Use OpenAI and X models.", owned: true, path: "/ai/icongpt" },
     { id: "manage-proxy", name: "👺 Proxy Management", type: "SERP", description: "Manage Proxy Endpoints.", owned: true, path: "/scraping-api/search-proxies" },
-    { id: "user-agents", name: "🕵️‍♂️ User Agents", type: "SERP", description: "Manage User Agents.", owned: true, path: "/scraping-api/user-agents" },
     { id: "google-serp", name: "🔍 Google SERP Management", type: "SERP", description: "Scrape real-time Google search results.", owned: true, path: "/scraping-api/google-serp" },
     { id: "cettire", name: "🔍 Cettire", type: "SERP", description: "Scrape Cettire search results.", owned: true, path: "/scraping-api/cettire" },
   ];
