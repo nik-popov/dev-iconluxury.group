f7ec87ac987d98f6643f27a6b65f523a0862bcf4
Nik Popov <149531476+nikwif@users.noreply.github.com>
2025-05-28 17:00:37 +0000

    sidebar links

--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -10,7 +10,7 @@ const theme = extendTheme({
   styles: {
     global: {
       "html, body": {
-        fontFamily: '"Futura", "Helvetica", "Arial", sans-serif',
+        fontFamily: '"Helvetica", "Arial", sans-serif',
         lineHeight: "1.7",
         bg: "gray.50",
         color: "gray.800",
